bf4d116d8685b2fb61c70abb70d1e002bb1e6b3a
Moses Hoggatt <endlessninetyeight@gmail.com>
2026-01-09 09:25:29 -0700

    Proper rendering of grid, and first pass at falling piece

--- TwinklyWall/games/tetris.py
+++ TwinklyWall/games/tetris.py
@@ -9,37 +9,94 @@ from logger import log
 from game_players import get_active_players_for_game, get_game_for_player
 from players import set_input_handler
 from pathlib import Path
+import copy
+import time
+
+
+class Tetrominoe:
+    def __init__(self, type_index, position = (0,0), rotation = 0):
+        self.position = position
+        self.shape = None
+        match type_index:
+            case 0: # I piece
+                self.shape = [[0,0,0,0],
+                              [1,1,1,1],
+                              [0,0,0,0],
+                              [0,0,0,0]]
+                              
+            case 1: # J piece
+                self.shape = [[0,0,0,0],
+                              [2,0,0,0],
+                              [2,2,2,0],
+                              [0,0,0,0]]
+            case 2: # L piece
+                self.shape = [[0,0,0,0],
+                              [0,0,0,3],
+                              [0,3,3,3],
+                              [0,0,0,0]]
+            case 3: # O piece
+                self.shape = [[0,0,0,0],
+                              [0,4,4,0],
+                              [0,4,4,0],
+                              [0,0,0,0]]
+            case 4: # S piece
+                self.shape = [[0,0,0,0],
+                              [0,0,5,5],
+                              [0,5,5,0],
+                              [0,0,0,0]]
+            case 5: # Z piece
+                self.shape = [[0,0,0,0],
+                              [6,6,0,0],
+                              [0,6,6,0],
+                              [0,0,0,0]]
+            case 6: # T piece
+                self.shape = [[0,0,0,0],
+                              [0,7,0,0],
+                              [7,7,7,0],
+                              [0,0,0,0]]
+
+
 
 class Tetris:
     def __init__(self, canvas, HEADLESS):
-        pygame.init() # Redundant?
-        
         self.headless = HEADLESS
         self.blocks_width = 10
-        self.blocks_height = 15 # Only 16.5 visible with current setup
+        self.blocks_height = 15 # Only 16.5 visible on matrix with current setup
         self.block_size = 3
         self.screen = canvas
         self.players = get_active_players_for_game
-
+        self.live_tetrominoe = Tetrominoe(random.randrange(0,6), position=(0,12))
+        self.is_playing = True
+        self.drop_interval_secs = 0.150
+        self.drop_time_elapsed = 0
+        
         # There is probably a more concise way to do this:
         project_dir = Path(__file__).resolve().parent
         assets_file_path = (str)(project_dir / "assets")
         self.block_images = [pygame.image.load(assets_file_path + "/TetrisSquare_Empty.png"),
+            pygame.image.load(assets_file_path + "/TetrisSquare_LightBlue.png"),
             pygame.image.load(assets_file_path + "/TetrisSquare_Blue.png"),
-            pygame.image.load(assets_file_path + "/TetrisSquare_Green.png"),
             pygame.image.load(assets_file_path + "/TetrisSquare_Orange.png"),
-            pygame.image.load(assets_file_path + "/TetrisSquare_Purple.png"),
+            pygame.image.load(assets_file_path + "/TetrisSquare_Yellow.png"),
+            pygame.image.load(assets_file_path + "/TetrisSquare_Green.png"),
             pygame.image.load(assets_file_path + "/TetrisSquare_Red.png"),
-            pygame.image.load(assets_file_path + "/TetrisSquare_Yellow.png")
+            pygame.image.load(assets_file_path + "/TetrisSquare_Purple.png"),
             ]
         for index in range(len(self.block_images)):
             self.block_images[index] = pygame.transform.scale(self.block_images[index], (self.block_size, self.block_size))
 
-        self.grid = [[]]
-        empty_block = "assets/tetris_blocks/TetrisSquare_Empty.png"
-        self.grid = [[random.randrange(0, len(self.block_images)) for element in range(self.blocks_width)] for row in range(self.blocks_height)]
-        print(self.screen.get_height())
+        # Random grid for debug
+        # self.dead_grid = [[random.randrange(0, len(self.block_images)) for element in range(self.blocks_height)] for row in range(self.blocks_width)]
+        self.dead_grid  = [[0 for element in range(self.blocks_height)] for row in range(self.blocks_width)]
+
+    def spawn_tetrominoe(self):
+        return Tetrominoe(random.randrange(0,6)) # Switch to 7 bag method later
    
+    def drop_tetrominoe_once(self):
+        if self.live_tetrominoe.position[1] <= -10: # TODO: Better logic for reaching floor or coming up against another piece
+            return
+        self.live_tetrominoe.position = (self.live_tetrominoe.position[0],) + (self.live_tetrominoe.position[1] - 1,)
+
     def bind_input(self):
         players = get_active_players_for_game("tetris")
         for i, player in enumerate(players):
@@ -48,45 +105,63 @@ class Tetris:
                     cmd = payload.get("cmd")
                     match cmd:
                         case "MOVE_LEFT":
-                            move_piece_left()
+                            self.move_piece_left()
                         case "MOVE_RIGHT":
-                            move_piece_right()
+                            self.move_piece_right()
                         case "ROTATE_CLOCKWISE":
-                            rotate_clockwise()
+                            self.rotate_clockwise()
                         case "ROTATE_COUNTERCLOCKWISE":
-                            rotate_counterclockwise()
+                            self.rotate_counterclockwise()
                         case "HARD_DROP":
-                            hard_drop_piece()
+                            self.hard_drop_piece()
                 return on_input
             set_input_handler(player.player_id, make_input_handler())
-        
        
-    def move_piece_left():
+    def move_piece_left(self):
         log("LEFT", module="Tetris")
-        
-    def move_piece_right():
+
+    def move_piece_right(self):
         log("RIGHT", module="Tetris")
 
-    def rotate_clockwise():
+    def rotate_clockwise(self):
         log("ROTATE_CLOCKWISE", module="Tetris")
 
-    def rotate_counterclockwise():
+    def rotate_counterclockwise(self):
         log("ROTATE__COUNTER_CLOCKWISE", module="Tetris")
 
-    def hard_drop_piece():
+    def hard_drop_piece(self):
         log("HARD_DROP_PIECE", module="Tetris")
 
-    def tick(self): # Called in main
-        
+    def begin_play(self): # Called in main
+        pass        
+
+    def tick(self, delta_time): # Called in main
+        self.drop_time_elapsed += delta_time
+        if self.drop_time_elapsed >= self.drop_interval_secs:
+            self.drop_tetrominoe_once()
+            self.drop_time_elapsed = 0
+
         if not self.headless:
             self.screen.fill((35,35,35)) # Help the preview pixels to stand out from the black background
             pygame.display.flip()
 
-        x_offset = self.screen.get_width() // self.block_size
-        y_offset = self.blocks_height - (self.screen.get_height() / (self.block_size))
-        # Draw cells
-        for y_index, row in enumerate(self.grid):
-            y_position = y_index - y_offset 
-            for x_index, value in enumerate(row):
-                x_position = x_offset - x_index - 1
-                self.screen.blit(self.block_images[self.grid[y_index][x_index]], (x_position * self.block_size, (y_position * self.block_size)) )
+        x_offset = self.screen.get_width() / self.block_size - self.blocks_width
+        y_offset = self.screen.get_height() / self.block_size - self.blocks_height - 1
+        # Draw dead cells
+        grid = copy.deepcopy(self.dead_grid) # Perform deep copy
+        # Draw tetrominoe on top of dead_grid
+        pos = self.live_tetrominoe.position
+        for local_x, grid_x in enumerate(range(pos[0], pos[0] + 4)):
+            for local_y, grid_y in enumerate(range(pos[1], pos[1] + 4)):
+                tetrominoe_cell_value = self.live_tetrominoe.shape[-local_y + 3][local_x] # Invert y because the origin is in the bottom left of the grid
+                if tetrominoe_cell_value != 0:
+                    grid[grid_x][grid_y] = tetrominoe_cell_value
+                
+        for x_index, row in enumerate(grid):
+            x_position = x_index + x_offset 
+            for y_index, value in enumerate(row): 
+                y_position = self.blocks_height - y_index + y_offset
+                # print(f"y_position: {y_position}")
+                # print(f"y_index: {y_index}")
+                self.screen.blit(self.block_images[grid[x_index][y_index]], (x_position * self.block_size, y_position * self.block_size))
+
